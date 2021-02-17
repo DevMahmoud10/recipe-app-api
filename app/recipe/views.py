4cb2b38d4d110f75127d87b52b95b0eae16ae884
mahmoud.ahmed <mahmoud.ahmed@benchmarklabs.io>
2021-02-17 18:25:05 +0200

    add: ingredients serializer

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,9 +2,9 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from CoreApp.models import Tag
+from CoreApp.models import Tag, Ingredient
 
-from recipe.serializers import TagSerializer
+from recipe.serializers import TagSerializer, IngredientSerializer
 
 
 class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
@@ -18,3 +18,13 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMo
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    serializer_class = IngredientSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
